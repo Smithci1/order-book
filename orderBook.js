36e570c00ec9c5777ff4a72336bfa224a0ee6bbd
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-13 15:05:14 -0400

    more attachments added

--- orderBook.js
+++ orderBook.js
@@ -2,6 +2,10 @@
 //I want to create an object array called orderbook
 let existingBook = orderBook
 let incomingOrder = order
+let type = orderBook.type
+let quantity = orderBook.quantity
+let price = orderBook.price
+
 const orderBook = [{
     
 
@@ -11,7 +15,7 @@ const orderBook = [{
 
 
 
-//inside of the object I will attach all identities from the test/readme to my object array
+//outside of the object I will attach all identities from the test/readme to my object array
 //out side of this object I will create my reconsile function
 //inside of this function I will deconstruct my orderbook to do all the right calculations
 //i will want to let orderBook equal updatedBook
