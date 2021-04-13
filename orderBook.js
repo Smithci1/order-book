61aa73c0b446e90cc46ad88fa2ee9d357d47d874
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-13 14:56:00 -0400

    suedo coded my initial approach to the project, also defined created the Orderbook

---
+++ orderBook.js
@@ -0,0 +1,17 @@
+//identify
+//I want to create an object array called orderbook
+let existingBook = orderBook
+let incomingOrder = order
+const orderBook = [{
+    
+
+}
+]
+
+
+
+
+//inside of the object I will attach all identities from the test/readme to my object array
+//out side of this object I will create my reconsile function
+//inside of this function I will deconstruct my orderbook to do all the right calculations
+//i will want to let orderBook equal updatedBook
