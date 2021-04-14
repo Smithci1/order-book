c938e7cc953ce492167684c4b8c983a2e58bc6b6
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-14 13:33:00 -0400

    i have deleted the orderbook object, and decided I wont need to make on

--- orderBook.js
+++ orderBook.js
@@ -1,11 +1,11 @@
 //identify
-//I want to create an object array called orderbook
+//outside of the object I will attach all identities from the test/readme to my object array
 let existingBook = orderBook
 let incomingOrder = order
 let type = orderBook.type
 let quantity = orderBook.quantity
 let price = orderBook.price
-
+//I want to create an object array called orderbook
 const orderBook = [{
     
 
@@ -15,7 +15,6 @@ const orderBook = [{
 
 
 
-//outside of the object I will attach all identities from the test/readme to my object array
 //out side of this object I will create my reconsile function
 //inside of this function I will deconstruct my orderbook to do all the right calculations
 //i will want to let orderBook equal updatedBook
