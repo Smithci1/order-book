e4a339037b0624de515613e033f637a023aeb9b1
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-15 13:49:17 -0400

    decided to use literal notation to destructure orderbook

--- orderBook.js
+++ orderBook.js
@@ -1,9 +1,12 @@
 // outside of my functions I will define,
 //  sotred orders and incoming orders
-let storedOrders = existingBook.length
-let incomingOrders = incomingOrder.length
-let buyPrice = existingBook.price
-let buyQuanity = existingBook.quantity
+let {type, quantity, price} = incomingOrders
+let existingBook = {type, quantity, price}
+// let storedOrders = existingBook.length
+
+// let incomingOrders = incomingOrder.length
+// let buyPrice = existingBook.price
+// let buyQuanity = existingBook.quantity
 // i will create a calculation function,
 //  for parcially fullfilled orders
 const partOrder
@@ -13,17 +16,41 @@ const partOrder
   for (let order = 0;
 order < array.length;
 order++) {
- if(incomingOrders.)   
+    let bookOrdertype = existingBook[order].type
+    let bookOrderprice = existingBook[order].price
+    let bookOrderquantity = existingBook[order].quantity
+    let newOrdertype = incomingOrders[order].type
+    let newOrderprice = incomingOrders[order].price
+    let newOrderQuantity = incomingOrders[order].quantity
+    //here i will say, if there is a new order to sell,
+    // and an existingorder to buy,
+    // and the quantities to buy are less
+    // than the quantities to sell and
+    // the price to but and sell are the same 
+    // fulfull that order.
+ if( newOrdertype === 'sell' &&
+     bookOrdertype === 'buy' &&
+     newOrderQuantity < bookOrderquantity &&
+     newOrderprice <= bookOrderprice)
+     return bookOrderquantity - newOrderQuantity &&
+     bookOrderprice - {
 
-  }
+
+     }
+      
+
+  
 
 //i will want to use push to add orders to the end of the array
 //i will no longer need to make an object.
-
+    }
+  }
 
 
 
 
 //out side of this object I will create my reconsile function
+
 //inside of this function I will deconstruct my orderbook to do all the right calculations
 //i will want to let orderBook equal updatedBook
+module.exports = reconcileOrder
