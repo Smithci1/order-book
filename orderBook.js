64afea7dcf8ef47cbf20b90db52a2b3adf2c7ae7
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-22 14:06:56 -0400

    updated function

--- orderBook.js
+++ orderBook.js
@@ -2,12 +2,13 @@
  //with incomingorder, existingorder
 
  const reconsileOrder = (existingBook,incomingOrder) => {
-  //  existingBook = []
-  //  if (existingBook.push(incomingOrder)){ 
-     
-  //  }
-  // existingBook = existingBook.push(incomingOrder)
+  
   for(let i = 0; i < existingBook.length; i++)
+  /* if the book is empty then input any incoming order,
+     if the book is not empyty compare the order type 
+     of the incoming order with all the orders in book 
+     and see if there is a deal to be made.
+  */
        if (existingBook === [])
        existingBook.push(incomingOrder) || 
          (existingBook[i].type !== 'buy' &&
