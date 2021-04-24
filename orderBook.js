8ea9ea4765d38e59e2a2831eb6a2e28f1d404994
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-24 12:18:05 -0400

    after alot of stress I took a different appproach

--- orderBook.js
+++ orderBook.js
@@ -1,44 +1,24 @@
-// const orderMatch = () => {
-//   for( let i = 0;
-//     i < existingBook.length;
-//     i++)
-// existingBook.shift(existingBook[i].type !== 
-//    incomingOrder.type &&
-//    existingBook[i].quantity.price === 
-//    incomingOrder.quantity.price)
+
+// const pusher = (existingBook, incomingOrder) => {
+//  for (let i = 0;
+//  i <existingBook.length;
+//  i++)
+//       if (existingBook === [])
+//       existingBook.push(incomingOrder) || 
+//          (existingBook[i].type !== incomingOrder.type)
+//          existingBook.push(incomingOrder) }
+
+const orderMatch =(existingOrder, incomingOrder) =>
+ existingOrder.type !== 
+ incomingOrder.type && 
+ existingOrder.price >= 
+ incomingOrder.price 
+const reconcileOrder = (existingBook, incomingOrder) => {
   
-// }
 
-const pusher = (existingBook, incomingOrder) => {
- for (let i = 0;
- i < existingBook.length;
- i++)
-      if (existingBook === [])
-       existingBook.push(incomingOrder) || 
-         (existingBook[i].type !== incomingOrder.type)
-          existingBook.push(incomingOrder) }
- const reconsileOrder = (existingBook,incomingOrder) => {
-      pusher(existingBook,incomingOrder)   
-          // (existingBook.filter(deal => 
-          //     deal.type !== 
-          //     incomingOrder.type) 
-          return existingBook
+}
 
-            
-           
-            
-           
-          
-            
-         
-          
-                 
-   }
-    
-    
- 
+module.exports = reconcileOrder
 
 
 
-
-module.exports = reconsileOrder
