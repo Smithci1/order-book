a5a9d3bdbe6062114e7b54019a277e1a05df07a4
Chadwick Smith <chadwickmini@Chadwicks-Mini.hsd1.ct.comcast.net>
2021-04-24 13:51:58 -0400

    test pass

--- orderBook.js
+++ orderBook.js
@@ -1,13 +1,4 @@
 
-// const pusher = (existingBook, incomingOrder) => {
-//  for (let i = 0;
-//  i <existingBook.length;
-//  i++)
-//       if (existingBook === [])
-//       existingBook.push(incomingOrder) || 
-//          (existingBook[i].type !== incomingOrder.type)
-//          existingBook.push(incomingOrder) }
-
 const orderMatch =(existingOrder, incomingOrder) =>
  existingOrder.type !== 
  incomingOrder.type && 
@@ -16,22 +7,23 @@ const orderMatch =(existingOrder, incomingOrder) =>
 const reconcileOrder = (existingBook, incomingOrder) => {
   //i will loop through book
   for (let i = 0; i< existingBook.length; i++){
-    if(orderMatch(existingBook[i], incomingOrder)&&
+    if(orderMatch(existingBook[i], incomingOrder) &&
                   existingBook[i].quantity === incomingOrder.quantity){
                     return existingBook.slice(0, i).concat(existingBook[i + 1])
                     .filter(worth => worth.quantity)
                   }else if (orderMatch(existingBook[i], incomingOrder) &&
-                            existingBook[i].quantity > incomingOrder.quantity){
-                              existingBook[i.quantity] - incomingOrder.quantity
-                              return existingBook.slice(0, i).concat(existingBook[i]
-                                .concat([existingBook[i + 1]]).filter(worth => worth.quantity))                            
+                                      existingBook[i].quantity > incomingOrder.quantity){
+                                      existingBook[i].quantity -= incomingOrder.quantity
+                                      return existingBook.slice(0, i).concat(existingBook[i + 1])
+                                      .concat([existingBook[i]]).filter(worth => worth.quantity)                            
 
-   }else if (orderMatch(existingBook[i], incomingOrder) &&
-    existingBook[i].quantity < incomingOrder.quantity){
-    incomingOrder.quantity -= existingBook[i].quantity
-    existingBook[i] = 0
-  } 
-} return existingBook.concat([incomingOrder]).filter(worth => worth.quantity)
+                  }else if (orderMatch(existingBook[i], incomingOrder) &&
+                                       existingBook[i].quantity < incomingOrder.quantity){
+                                       incomingOrder.quantity -= existingBook[i].quantity
+                                       existingBook[i] = 0
+                                      } 
+                                    }return existingBook.concat([incomingOrder])
+                                            .filter(worth => worth.quantity)
 }
 
 module.exports = reconcileOrder
